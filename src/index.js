5e37f707fc3e570fee02dd845d3fb11a7ac10d87
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-08 23:49:37 +0530

    chore : added react-18 dependency

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { makeServer } from "./server";
 
 // Call make Server
 makeServer();
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
