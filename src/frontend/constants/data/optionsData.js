d96f9f19b4451a35fc0b49e0a9d6d69413d213e2
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-09 20:23:01 +0530

    created constant nav options data

---
+++ src/frontend/constants/data/optionsData.js
@@ -0,0 +1,25 @@
+import {
+    RiPlayList2Fill,
+    AiOutlineLike,
+    MdOutlineWatchLater,
+    RiHistoryLine,
+  } from "../react-icons";
+
+export const optionsData = [
+  {
+    Icon: <RiPlayList2Fill />,
+    title: "Playlist",
+  },
+  {
+    Icon: <AiOutlineLike />,
+    title: "Liked",
+  },
+  {
+    Icon: <MdOutlineWatchLater />,
+    title: "WatchLater",
+  },
+  {
+    Icon: <RiHistoryLine />,
+    title: "History",
+  },
+];
