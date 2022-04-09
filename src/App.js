f0ddeb32b0c16f91dc6f61a800967a1db8f1ffe8
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-09 18:56:06 +0530

    feat :```Header``` component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Header } from "./frontend/Layout/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      {/* Header */}
+      <Header />
       {/* main */}
       {/* Footer */}
     </div>
---
+++ src/frontend/Layout/Header/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  RiPlayList2Fill,
+  AiOutlineLike,
+  MdOutlineWatchLater,
+  RiHistoryLine,
+  MdOutlineSearch,
+  CgProfile,
+  IoMdArrowDropdown 
+} from "../../constants/react-icons";
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <header className="flex-row header-bar spc-btwn">
+      <div className="flex-row header-left-content gap-btwn">
+        <img className="web-logo-img" src="./assets/gamseylogo.jpg" alt="" />
+        <h1 className="flex-row web-logo">Gamsey</h1>
+        <div className="flex-row gap-btwn header-options">
+          <div className="flex-col center-it">
+            <RiPlayList2Fill />
+            <span>Playlist</span>
+          </div>
+          <div className="flex-col center-it">
+            <AiOutlineLike />
+            <span>Liked</span>
+          </div>
+          <div className="flex-col center-it">
+            <MdOutlineWatchLater />
+            <span>WatchLater</span>
+          </div>
+          <div className="flex-col center-it">
+            <RiHistoryLine />
+            <span>History </span>
+          </div>
+        </div>
+        <div className="dropdown-menu">
+        <span>Browse</span>
+        <IoMdArrowDropdown  className="dropdown-icon"/>
+        </div>
+      </div>
+
+      <div className="flex-row center-it gap-btwn header-right-content">
+        <div className="search-wrapper">
+          <MdOutlineSearch className="search-icon" />
+          <input type="text" placeholder="Search " className="search-bar" />
+        </div>
+        <CgProfile className="profile-icon" />
+      </div>
+    </header>
+  );
+};
