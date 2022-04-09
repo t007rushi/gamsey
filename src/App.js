6502cf86b5c25943f9fdb37ab3314413adf18758
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-10 01:05:28 +0530

    main component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import { Header } from "./frontend/Layout/Header/Header";
+import { Main } from "./frontend/Layout/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* main */}
+      <Main />
       {/* Footer */}
     </div>
   );
