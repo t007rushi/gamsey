14d84c1419d2223122e1f443456e988209d856b8
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-09 18:56:57 +0530

    created react-icons file

---
+++ src/frontend/constants/react-icons.js
@@ -0,0 +1,5 @@
+export {RiPlayList2Fill,RiHistoryLine} from "react-icons/ri"
+export { AiOutlineLike } from "react-icons/ai"
+export { MdOutlineWatchLater,MdOutlineSearch } from "react-icons/md"
+export { CgProfile } from "react-icons/cg"
+export { IoMdArrowDropdown } from "react-icons/io"
