2633baea7513427b58e32c20dcd7a68a13e671d1
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-11 15:27:28 +0530

    added data into videos and category DB

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -1,3 +1,5 @@
+import { v4 as uuid } from "uuid";
+
 /**
  * Videos Database can be added here.
  * You can add videos of your wish with different attributes
@@ -5,17 +7,454 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/pc/farcry6_KgK7f2lbZE.png?ik-sdk-version=javascript-1.4.3&updatedAt=1649669798331",
+    categoryName: "pc",
+    genre: "action",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/pc/reddead2v_rqZsscIqT.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669797684",
+    categoryName: "pc",
+    genre: "action",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/pc/valhala_zn5bOOcjM.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669797243",
+    categoryName: "pc",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/pc/minecraft_z6L-K5UqE.png?ik-sdk-version=javascript-1.4.3&updatedAt=1649669795376",
+    categoryName: "pc",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/pc/pubg_RAxWnxw_r.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669795632",
+    categoryName: "pc",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/pc/gta_Nd5lL3uZT.png?ik-sdk-version=javascript-1.4.3&updatedAt=1649669795218",
+    categoryName: "pc",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/pc/lolegend_JFKu8MAhC.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669794919",
+    categoryName: "pc",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/pc/Cover-art-6_OBK4HFwYY.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669794228",
+    categoryName: "pc",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/pc/fortnite_De6qWbSxei.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669794318",
+    categoryName: "pc",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/pc/elden_ekRhF_3dh.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669794142",
+    categoryName: "pc",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps4/uc_4_thiefs_end_w3rtjQOMj.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669885170",
+    categoryName: "ps4",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps4/grand_FcLk0K0mY.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669884230",
+    categoryName: "ps4",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps4/lastofus2_KQI_ffplq.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669884741",
+    categoryName: "ps4",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps4/gotg_me05m6s8z.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669884358",
+    categoryName: "ps4",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps4/maxresdefault_UlW6dryodA.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669884823",
+    categoryName: "ps4",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps4/gos2_8LqsRHSCd.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669883172",
+    categoryName: "ps4",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps4/final2_ZiPhWsnx8.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669882725",
+    categoryName: "ps4",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps4/fifa_Y8XFkJBXf.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669882981",
+    categoryName: "ps4",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps4/cod-black-ops-cold-war-cover-1597947738556-1_1qkq_BbD8xa5t3.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669881450",
+    categoryName: "ps4",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps4/destiny_HcWwV_8df.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669881792",
+    categoryName: "ps4",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps5/deathloop_image_dajIZg2zu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669899656",
+    categoryName: "ps5",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps5/deathloop_image_dajIZg2zu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669899656",
+    categoryName: "ps5",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps5/deathloop_image_dajIZg2zu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669899656",
+    categoryName: "ps5",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps5/deathloop_image_dajIZg2zu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669899656",
+    categoryName: "ps5",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps5/deathloop_image_dajIZg2zu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669899656",
+    categoryName: "ps5",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps5/deathloop_image_dajIZg2zu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669899656",
+    categoryName: "ps5",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps5/deathloop_image_dajIZg2zu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669899656",
+    categoryName: "ps5",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps5/deathloop_image_dajIZg2zu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669899656",
+    categoryName: "ps5",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps5/deathloop_image_dajIZg2zu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669899656",
+    categoryName: "ps5",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/ps5/deathloop_image_dajIZg2zu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1649669899656",
+    categoryName: "ps5",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/xbox/halo_pXxZDHd6_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669918874",
+    categoryName: "xbox",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/xbox/halo_pXxZDHd6_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669918874",
+    categoryName: "xbox",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/xbox/halo_pXxZDHd6_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669918874",
+    categoryName: "xbox",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/xbox/halo_pXxZDHd6_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669918874",
+    categoryName: "xbox",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/xbox/halo_pXxZDHd6_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669918874",
+    categoryName: "xbox",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/xbox/halo_pXxZDHd6_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669918874",
+    categoryName: "xbox",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/xbox/halo_pXxZDHd6_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669918874",
+    categoryName: "xbox",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/xbox/halo_pXxZDHd6_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669918874",
+    categoryName: "xbox",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/xbox/halo_pXxZDHd6_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669918874",
+    categoryName: "xbox",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/xbox/halo_pXxZDHd6_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669918874",
+    categoryName: "xbox",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
+  },
+  {
+    _id: uuid(),
+    title: "god of war",
+    image:
+      "https://ik.imagekit.io/t007rushi/VideoCardImages/xbox/halo_pXxZDHd6_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1649669918874",
+    categoryName: "xbox",
+    genre: "action",
+    description:
+      "God of War is an action-adventure game franchise created by David Jaffe at Sony's Santa Monica Studio. ",
+    creator: "Rushikesh Tarapure",
   },
 ];
