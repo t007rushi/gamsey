9d4934cd6fd46ffbe2f985202c059c07c9139ca5
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-13 01:04:31 +0530

    fix : fixed the slider styling

--- src/frontend/components/Row/Row.jsx
+++ src/frontend/components/Row/Row.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { useVideos } from "../../context/videos-context";
 import "./row.css";
 import {
@@ -7,46 +7,53 @@ import {
   MdArrowBackIos,
 } from "../../constants/react-icons";
 
-export const Row = ({cat,title}) => {
+export const Row = ({ cat, title }) => {
   const { videos, fetchVideos } = useVideos();
+  const [val, setVal] = useState(0);
   useEffect(() => {
     fetchVideos();
   }, []);
   const pcGames = videos.filter((vid) => vid.categoryName === cat);
-
-  const listRef = useRef();
   const ArrowHandler = (dir) => {
     if (dir === "right") {
-      listRef.current.style.transform = `translateX(100px)`;
+      setVal((v) => v - 50);
     }
     if (dir === "left") {
-        listRef.current.style.transform = `translateX(-100px)`;
-      }
+      setVal((v) => v + 50);
+    }
   };
 
   return (
-      <>
-      
-    <h1 className="row-title">{title}</h1>
-    <div className="row-wrapper" >
-      <MdArrowBackIos
-        className="sliderarrow backward-arrow"
-        onClick={() => ArrowHandler("left")}
-      />
-      <div className="row-content" ref={listRef}>
-        {pcGames.map((item) => {
-          return (
-            <div key={item._id} className="title_wrap" >
-              <img key={item._id} src={item.image} alt="images" className="title_poster" />
-            </div>
-          );
-        })}
+    <>
+      <h1 className="row-title">{title}</h1>
+      <div className="row-wrapper">
+        <MdArrowBackIos
+          className="sliderarrow backward-arrow"
+          onClick={() => ArrowHandler("left")}
+        />
+        <div className="row-content">
+          {pcGames.map((item) => {
+            return (
+              <div
+                key={item._id}
+                className="title_wrap"
+                style={{ transform: `translateX(${val}rem)` }}
+              >
+                <img
+                  key={item._id}
+                  src={item.image}
+                  alt="images"
+                  className="title_poster"
+                />
+              </div>
+            );
+          })}
+        </div>
+        <MdOutlineArrowForwardIos
+          className="sliderarrow forward-arrow"
+          onClick={() => ArrowHandler("right")}
+        />
       </div>
-      <MdOutlineArrowForwardIos
-        className="sliderarrow forward-arrow"
-        onClick={() => ArrowHandler("right")}
-      />
-    </div>
     </>
   );
 };
