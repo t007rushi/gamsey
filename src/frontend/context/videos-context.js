49a26b84e07710be30d52205df038a274ebf128c
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-12 17:15:54 +0530

    ```video context``` created

---
+++ src/frontend/context/videos-context.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useState, createContext, useContext } from "react";
+
+const videoContext = createContext();
+
+const VideoProvider = ({ children }) => {
+  const [videos, setVideos] = useState([]);
+
+  //Get videos
+  const url = "/api/videos";
+  const fetchVideos = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setVideos(data.videos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <videoContext.Provider value={{ videos, fetchVideos }}>
+      {children}
+    </videoContext.Provider>
+  );
+};
+
+const useVideos = () => {
+  const data = useContext(videoContext);
+  if (data !== undefined) {
+    return data;
+  }
+};
+
+export { useVideos, VideoProvider };
