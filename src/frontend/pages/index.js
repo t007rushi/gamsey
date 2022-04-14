d188ae747cbb564c541bb135dd3da510240ae402
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-14 16:05:06 +0530

    exported

--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -1,1 +1,7 @@
 export { HomePage } from "./Home/HomePage";
+export { LoginPage } from "./Login/LoginPage";
+export { SignupPage } from "./Signup/SignupPage";
+export { History } from "./History/History";
+export { Liked } from "./Liked/Liked";
+export { PlayList } from "./PlayList/PlayList";
+export { WatchLater } from "./WatchLater/WatchLater";
