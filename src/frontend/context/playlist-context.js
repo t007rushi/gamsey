a1e2f124a8c46e67391557616fdd0131011b11af
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-17 22:06:36 +0530

    playlist context created

---
+++ src/frontend/context/playlist-context.js
@@ -0,0 +1,48 @@
+import { useState, createContext, useContext,useReducer } from "react";
+import { PlaylistReducer } from "../reducer/playlist-reducer";
+import { getPlaylist } from "../services/playlist/getPlaylist";
+import { useAuth } from "./auth-context";
+
+const PlaylistContext = createContext();
+
+const PlaylistProvider = ({ children }) => {
+  const [Playlists, setPlaylists] = useState([]);
+  const {
+    user: { tokenVal },
+  } = useAuth();
+
+  const initialState = {
+    playlists: [],
+    likes:[]
+  };
+  
+
+  const [playlistState,playlistDipatcher] = useReducer(PlaylistReducer,initialState)
+  console.log(playlistState)
+
+  //Get Playlists
+  const fetchPlaylists = async () => {
+    try {
+      const { data } = await getPlaylist(tokenVal);
+      setPlaylists(data.playlists);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ 
+
+  return (
+    <PlaylistContext.Provider value={{ Playlists, fetchPlaylists,playlistState,playlistDipatcher }}>
+      {children}
+    </PlaylistContext.Provider>
+  );
+};
+
+const usePlaylists = () => {
+  const data = useContext(PlaylistContext);
+  if (data !== undefined) {
+    return data;
+  }
+};
+
+export { usePlaylists, PlaylistProvider };
