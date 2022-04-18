ee8d0ed67bb6f9dacc27137b48fee23210ded97d
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-18 23:22:50 +0530

    fix:  fixed id specific issue fixed

--- src/frontend/services/playlist/deletePlaylist.js
+++ src/frontend/services/playlist/deletePlaylist.js
@@ -1,16 +1,12 @@
-
 import axios from "axios";
 
 export const createNewPlaylist = async (playlistId, token) => {
   try {
-    const {data} = await axios.delete(
-      `/api/user/playlists/:${playlistId}`,
-      {
-        headers: {
-          authorization: token,
-        },
-      } 
-    );
+    const { data } = await axios.delete(`/api/user/playlists/${playlistId}`, {
+      headers: {
+        authorization: token,
+      },
+    });
     return data;
   } catch (err) {
     console.log(err);
