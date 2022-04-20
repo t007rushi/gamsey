ed33ef1873539b9b7b22adb7430f3fbff7bd75ad
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-20 18:56:46 +0530

    feat: cases added add/rmv video ,delete playlist

--- src/frontend/reducer/playlist-reducer.js
+++ src/frontend/reducer/playlist-reducer.js
@@ -1,10 +1,42 @@
-import { CREATE_PLAYLIST } from "../constants/playlist-constants";
+import {
+  ADD_TO_PLAYLIST,
+  CREATE_PLAYLIST,
+  REMOVE_FROM_PLAYLIST,
+  REMOVE_PLAYLIST,
+} from "../constants/playlist-constants";
 
 export const PlaylistReducer = (state, action) => {
   switch (action.type) {
     case CREATE_PLAYLIST: {
       return { ...state, playlists: action.payload };
     }
+    case REMOVE_PLAYLIST:
+      return { ...state, playlists: action.payload };
+    case ADD_TO_PLAYLIST:
+      return {
+        ...state,
+        playlists: state.playlists.map((playlistVid) =>
+          playlistVid._id === action.payload._id
+            ? {
+                ...playlistVid,
+                
+                videos: action.payload.videos,
+              }
+            : playlistVid
+        ),
+      };
+      case REMOVE_FROM_PLAYLIST:
+     return   {
+          ...state,
+          playlists: state.playlists.map((playlistVid) =>
+            playlistVid._id === action.payload._id
+              ? {
+                  ...playlistVid,
+                  videos: action.payload.videos,
+                }
+              : playlistVid
+          ),
+        };
     default:
       return state;
   }
