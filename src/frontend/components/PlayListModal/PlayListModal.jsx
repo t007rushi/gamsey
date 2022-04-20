820f52d5506c25dc9cd768e8a1bd0836f36926f8
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-20 19:14:55 +0530

    playlist modal update

--- src/frontend/components/PlayListModal/PlayListModal.jsx
+++ src/frontend/components/PlayListModal/PlayListModal.jsx
@@ -1,30 +1,88 @@
 import React from "react";
 import "./playlistmodal.css";
-import { GrClose } from "../../constants/react-icons";
+import { GrClose, MdDeleteOutline } from "../../constants/react-icons";
 import { CreatePlaylist } from "../CreatePlaylist/CreatePlaylist";
 import { usePlaylists } from "../../context/playlist-context";
+import { deletePlaylist } from "../../services/playlist/deletePlaylist";
+import {
+  ADD_TO_PLAYLIST,
+  REMOVE_FROM_PLAYLIST,
+  REMOVE_PLAYLIST,
+} from "../../constants/playlist-constants";
+import { useAuth } from "../../context/auth-context";
+import { addSpecificVideoToPlaylist } from "../../services/playlist/addSpecificVideoToPlaylist";
+import { deleteSpecificVideoFromPlaylist } from "../../services/playlist/deleteSpecificVideoFromPlaylist";
 
-export const PlayListModal = ({ closeFun }) => {
+export const PlayListModal = ({ closeFun, vid }) => {
+  const { playlistDipatcher } = usePlaylists();
   const {
-    playlistState:{playlists},
+    user: { tokenVal },
+  } = useAuth();
+  const handleDeletePlaylist = async (id) => {
+    try {
+      const data = await deletePlaylist(id, tokenVal);
+      playlistDipatcher({ type: REMOVE_PLAYLIST, payload: data.playlists });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const {
+    playlistState: { playlists },
   } = usePlaylists();
+
+  const addVideoToPlaylist = async (id, vid) => {
+    try {
+      const data = await addSpecificVideoToPlaylist(id, vid, tokenVal);
+      playlistDipatcher({ type: ADD_TO_PLAYLIST, payload: data.playlist });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const deleteVideoFromPlaylist = async (id, vid_id) => {
+    try {
+      const data = await deleteSpecificVideoFromPlaylist(id, vid_id, tokenVal);
+      playlistDipatcher({ type: REMOVE_FROM_PLAYLIST, payload: data.playlist });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log(playlists);
   return (
     <div className="modal-container">
       <div className=" flex-col modal">
         <div className="flex-row spc-btwn modal-content">
-          {" "}
-          SAVE TO...{" "}
+          SAVE TO...
           <span onClick={closeFun}>
             <GrClose />
           </span>
         </div>
         <div className="flex-col gap-btwn playlist-collection-container">
-          {playlists.map(play =>{
-            return(
-              <label className="flex-row gap-btwn center-it" key={play._id}>
-              <input type="checkbox" name="" id=""/> {play.title}
-            </label>
-            )
+          {playlists.map((play) => {
+            return (
+              <div className="flex-row spc-btwn playlist-item" key={play._id}>
+                <label className="flex-row gap-btwn center-it">
+                  <input
+                    type="checkbox"
+                    name=""
+                    id=""
+                    checked={play.videos.some((video) => {
+                      return video._id === vid._id;
+                    })}
+                    onChange={(e) => {
+                      if (e.target.checked) {
+                        addVideoToPlaylist(play._id, vid);
+                      } else {
+                        deleteVideoFromPlaylist(play._id, vid._id);
+                      }
+                    }}
+                  />{" "}
+                  {play.title}
+                </label>
+                <MdDeleteOutline
+                  onClick={() => handleDeletePlaylist(play._id)}
+                />
+              </div>
+            );
           })}
         </div>
         <CreatePlaylist />
