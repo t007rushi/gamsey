a99cbf542e1afd52d4ba08455eb2ffc1e0512f13
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-22 18:11:27 +0530

    like reducer created with cases

---
+++ src/frontend/reducer/like-reducer.js
@@ -0,0 +1,12 @@
+import { ADD_TO_LIKE, RMV_FROM_LIKE } from "../constants/like-constants";
+
+export const LikeReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_TO_LIKE:
+      return { ...state, likes: action.payload };
+    case RMV_FROM_LIKE:
+      return { ...state, likes: action.payload };
+    default:
+      return state;
+  }
+};
