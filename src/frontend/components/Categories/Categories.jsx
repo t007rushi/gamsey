c331b1226b5237daaad6670eb6db6681f4c16180
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-22 18:08:17 +0530

    fix: category ```key prop``` warning fixed

--- src/frontend/components/Categories/Categories.jsx
+++ src/frontend/components/Categories/Categories.jsx
@@ -3,15 +3,14 @@ import { categoryData } from "../../constants/data/categoryViewData";
 import "./categories.css";
 
 export const Categories = () => {
-  
   return (
     <div className="category-container">
-      {categoryData.map(({categoryName,image,video}) => {
+      {categoryData.map(({ id, image, video }) => {
         return (
-          <div key={categoryName} className="category-wrapper">
+          <div key={id} className="category-wrapper">
             <img src={image} alt="items" className="category" />
             <video autoPlay={true} loop={true} playsInline={true} muted={true}>
-                <source src={video} type="video/mp4"/>
+              <source src={video} type="video/mp4" />
             </video>
           </div>
         );
