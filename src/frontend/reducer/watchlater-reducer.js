471700c3e2da5f383c23c0f3ea6ae9241de2f98b
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-22 18:11:38 +0530

    watchlater reducer created with cases

---
+++ src/frontend/reducer/watchlater-reducer.js
@@ -0,0 +1,15 @@
+import {
+  ADD_TO_WATCHLATER,
+  RMV_FROM_WATCHLATER,
+} from "../constants/watchlater-constants";
+
+export const WatchlaterReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_TO_WATCHLATER:
+      return { ...state, watchlater: action.payload };
+    case RMV_FROM_WATCHLATER:
+      return { ...state, watchlater: action.payload };
+    default:
+      return state;
+  }
+};
