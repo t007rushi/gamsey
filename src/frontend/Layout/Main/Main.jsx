1014eed1d07ab70427db0e8462494ece71dc63e2
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-23 14:05:54 +0530

    protectedRoute => Require Auth

--- src/frontend/Layout/Main/Main.jsx
+++ src/frontend/Layout/Main/Main.jsx
@@ -12,7 +12,7 @@ import {
   SignupPage,
 } from "../../pages/index";
 import { WatchLater } from "../../pages/WatchLater/WatchLater";
-import ProtectedRoute from "../../router/ProtectedRoutes";
+import RequireAuth from "../../router/RequireAuth";
 
 export const Main = () => {
   const {
@@ -32,31 +32,44 @@ export const Main = () => {
         <Route
           path="/playlist"
           element={
-            <ProtectedRoute ProtectedComp={<PlayList />}></ProtectedRoute>
+            <RequireAuth>
+              <PlayList />
+            </RequireAuth>
           }
         />
         <Route
           path="/playlist/:playlistID"
           element={
-            <ProtectedRoute ProtectedComp={<PlaylistVideos />}></ProtectedRoute>
+            <RequireAuth>
+              <PlaylistVideos />
+            </RequireAuth>
           }
         />
 
         <Route
           path="/liked"
-          element={<ProtectedRoute ProtectedComp={<Liked />}></ProtectedRoute>}
+          element={
+            <RequireAuth>
+              <Liked />
+            </RequireAuth>
+          }
         />
 
         <Route
           path="/watchlater"
           element={
-            <ProtectedRoute ProtectedComp={<WatchLater />}></ProtectedRoute>
+            <RequireAuth>
+              <WatchLater />
+            </RequireAuth>
           }
         />
+
         <Route
           path="/history"
           element={
-            <ProtectedRoute ProtectedComp={<History />}></ProtectedRoute>
+            <RequireAuth>
+              <History />
+            </RequireAuth>
           }
         />
         <Route path="/explore" element={<Explore />} />
