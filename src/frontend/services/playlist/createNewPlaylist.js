001606e8f28456b3b4967e03525b9c4288117eaf
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-06-18 17:26:19 +0530

    ```toast``` added

--- src/frontend/services/playlist/createNewPlaylist.js
+++ src/frontend/services/playlist/createNewPlaylist.js
@@ -1,18 +1,21 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 export const createNewPlaylist = async (playlistData, token) => {
   try {
-    const {data} = await axios.post(
+    const { data } = await axios.post(
       "/api/user/playlists",
       { playlist: playlistData },
       {
         headers: {
           authorization: token,
         },
-      } 
+      }
     );
+    toast.success(`Playlist created successfully`);
     return data;
   } catch (err) {
     console.log(err);
+    toast.error("Error while creating playlist");
   }
 };
